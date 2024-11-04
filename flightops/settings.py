f7becb8b927adccd27681eee78b2e862166c4b79
eliekayitare <kayitarelie@gmail.com>
2024-11-04 13:20:51 +0200

    new commit

--- flightops/settings.py
+++ flightops/settings.py
@@ -80,10 +80,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Allowed Hosts
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 
 AD_AUTH_URL = config('AD_AUTH_URL')
 # Application definition
