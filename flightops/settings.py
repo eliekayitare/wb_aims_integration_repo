f6472e59e06b2244f2a491ad8910df3237fc6cfb
eliekayitare <kayitarelie@gmail.com>
2024-11-05 20:31:10 +0200

    new commit

--- flightops/settings.py
+++ flightops/settings.py
@@ -78,7 +78,7 @@ LOGGING = {
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['10.0.0.120', 'localhost', '127.0.0.1','wbhub.rwandair.com']
 
