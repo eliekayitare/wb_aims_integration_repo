10195be89987c19550157db8b4a99ab99b010321
eliekayitare <kayitarelie@gmail.com>
2024-11-05 20:25:13 +0200

    new commit

--- flightops/settings.py
+++ flightops/settings.py
@@ -160,18 +160,6 @@ DATABASES = {
     },
 }
 
-# Enforce HTTPS
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_HSTS_SECONDS = 31536000  # Adjust the max-age as necessary
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
-
-# Allow the Nginx proxy to handle headers correctly
-USE_X_FORWARDED_HOST = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 
 
 # Password validation
