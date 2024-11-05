2d5d2c3112027eaabc0f37a9182fd90e31b0f479
eliekayitare <kayitarelie@gmail.com>
2024-11-05 16:57:56 +0200

    new commit

--- aimsintegration/views.py
+++ aimsintegration/views.py
@@ -1,34 +1,29 @@
 
 
-from django.shortcuts import render
-from .models import FlightData
-from datetime import date
-from datetime import timedelta
+# from django.shortcuts import render
+# from .models import FlightData
+# from datetime import date
+# from datetime import timedelta
+
+
 
 # def dashboard_view(request):
 #     # Fetch only today's flight data from the database
 #     today = date.today()  # Get today's date
-#     yesterday = today - timedelta(days=1)  # Get yesterday's date
 
-# #    display only today's flightsl and yesterday's flights
-
-#     schedules = FlightData.objects.filter(date_utc__in=[today, yesterday]).order_by('-sd_date_utc')
+#     #display only today's flights
+#     schedules = FlightData.objects.filter(sd_date_utc=today).order_by('-sd_date_utc')
 #     return render(request, 'aimsintegration/dashboard.html', {'schedules': schedules})
-
+    
+from django.shortcuts import render
+from .models import FlightData
+from datetime import date
 
 def dashboard_view(request):
     # Fetch only today's flight data from the database
     today = date.today()  # Get today's date
 
-    #display only today's flights
+    # Display only today's flights
     schedules = FlightData.objects.filter(sd_date_utc=today).order_by('-sd_date_utc')
-    return render(request, 'aimsintegration/dashboard.html', {'schedules': schedules})
     
-
-# def dashboard_view(request):
-#     # Fetch only today's flight data from the database
-#     today = date.today()  # Get today's date
-#     yesterday = today - timedelta(days=1)  # Get yesterday's date
-#     #display only today's flights
-#     schedules = FlightData.objects.filter(sd_date_utc=yesterday)
-#     return render(request, 'aimsintegration/dashboard.html', {'schedules': schedules})
+    return render(request, 'aimsintegration/dashboard.html', {'schedules': schedules})
