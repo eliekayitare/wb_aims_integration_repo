8ad6d561066c2eb21a8348cdace6e71dca86614d
eliekayitare <kayitarelie@gmail.com>
2024-11-05 17:02:39 +0200

    new commit

--- aimsintegration/views.py
+++ aimsintegration/views.py
@@ -1,29 +1,17 @@
 
 
-# from django.shortcuts import render
-# from .models import FlightData
-# from datetime import date
-# from datetime import timedelta
-
-
-
-# def dashboard_view(request):
-#     # Fetch only today's flight data from the database
-#     today = date.today()  # Get today's date
-
-#     #display only today's flights
-#     schedules = FlightData.objects.filter(sd_date_utc=today).order_by('-sd_date_utc')
-#     return render(request, 'aimsintegration/dashboard.html', {'schedules': schedules})
-    
 from django.shortcuts import render
 from .models import FlightData
 from datetime import date
+from datetime import timedelta
+
+
 
 def dashboard_view(request):
     # Fetch only today's flight data from the database
     today = date.today()  # Get today's date
 
-    # Display only today's flights
+    #display only today's flights
     schedules = FlightData.objects.filter(sd_date_utc=today).order_by('-sd_date_utc')
-    
     return render(request, 'aimsintegration/dashboard.html', {'schedules': schedules})
+    
