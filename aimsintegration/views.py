7030510a1045c9654204e538c8b8ef6344fa948c
eliekayitare <kayitarelie@gmail.com>
2024-11-06 15:54:03 +0200

    new commit

--- aimsintegration/views.py
+++ aimsintegration/views.py
@@ -1,39 +1,13 @@
 
 
-# from django.shortcuts import render
-# from .models import FlightData
-# from datetime import date
-# from datetime import timedelta
-
-# def dashboard_view(request):
-#     # Fetch only today's flight data from the database
-#     today = date.today()  # Get today's date
-#     print("=================================")
-#     print(today)
-#     print("==================================")
-#     #display only today's flights
-#     schedules = FlightData.objects.filter(sd_date_utc=today)
-#     print("==================================")
-#     print(schedules.sd_date_utc)
-#     print("==================================")
-#     return render(request, 'aimsintegration/dashboard.html', {'schedules': schedules})
-    
 from django.shortcuts import render
 from .models import FlightData
-from datetime import date, timedelta
+from datetime import date
+from datetime import timedelta
 
 def dashboard_view(request):
     # Fetch only today's flight data from the database
     today = date.today()  # Get today's date
-    print("=================================")
-    print(today)
-    print("==================================")
-    
-    # Display only today's flights
+    #display only today's flights
     schedules = FlightData.objects.filter(sd_date_utc=today)
-    print("==================================")
-    for schedule in schedules:
-        print(schedule.sd_date_utc)  # Access each record's sd_date_utc attribute
-    print("==================================")
-    
     return render(request, 'aimsintegration/dashboard.html', {'schedules': schedules})
