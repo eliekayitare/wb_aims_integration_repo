dca33dddb8434f4e1354b6240ea31ee330c5a85f
eliekayitare <kayitarelie@gmail.com>
2024-11-06 15:13:22 +0200

    new commit

--- aimsintegration/views.py
+++ aimsintegration/views.py
@@ -5,13 +5,16 @@ from .models import FlightData
 from datetime import date
 from datetime import timedelta
 
-
-
 def dashboard_view(request):
     # Fetch only today's flight data from the database
     today = date.today()  # Get today's date
-
+    print("=================================")
+    print(today)
+    print("==================================")
     #display only today's flights
     schedules = FlightData.objects.filter(sd_date_utc=today).order_by('-sd_date_utc')
+    print("==================================")
+    print(schedules.sd_date_utc)
+    print("==================================")
     return render(request, 'aimsintegration/dashboard.html', {'schedules': schedules})
     
