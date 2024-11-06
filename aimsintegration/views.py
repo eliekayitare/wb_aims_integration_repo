798d1246f7eacc0651e48b15462a1ede0ea66a0c
eliekayitare <kayitarelie@gmail.com>
2024-11-06 12:39:57 +0200

    new commit

--- aimsintegration/views.py
+++ aimsintegration/views.py
@@ -1,18 +1,17 @@
 
 
-from django.utils import timezone
 from django.shortcuts import render
 from .models import FlightData
+from datetime import date
+from datetime import timedelta
+
+
 
 def dashboard_view(request):
-    # Fetch only today's flight data in UTC
-    today = timezone.now().date()  # Get today's date in UTC
-    print("====================")
-    print(f"Today's date: {today}")
-    print("====================")
-    # Display only today's flights
+    # Fetch only today's flight data from the database
+    today = date.today()  # Get today's date
+
+    #display only today's flights
     schedules = FlightData.objects.filter(sd_date_utc=today).order_by('-sd_date_utc')
-    print("====================")
-    print(f"sd_date_utc: {schedules.sd_date_utc}")
-    print("====================")
     return render(request, 'aimsintegration/dashboard.html', {'schedules': schedules})
+    
