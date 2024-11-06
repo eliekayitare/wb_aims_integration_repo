eddd38d0e9274aeeced4cacf93ac02cdc30cf5f0
eliekayitare <kayitarelie@gmail.com>
2024-11-06 16:35:37 +0200

    new

--- flightops/urls.py
+++ flightops/urls.py
@@ -14,17 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import include, path
-# from django.conf import settings
-# from django.conf.urls.static import static
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', include('authentications.urls')), 
-#     path('aims/', include('aimsintegration.urls')),
-# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 
 from django.contrib import admin
 from django.urls import include, path
