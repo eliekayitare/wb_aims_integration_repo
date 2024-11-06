3d7041f4d664ae64089595db2dfef9b91800a554
eliekayitare <kayitarelie@gmail.com>
2024-11-06 16:39:50 +0200

    new

--- aimsintegration/urls.py
+++ aimsintegration/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard_view, name='dashboard'),
-    path('search-flights/', views.search_flights, name='search_flights'),  # Add this line
 ]
--- aimsintegration/views.py
+++ aimsintegration/views.py
@@ -12,37 +12,3 @@ def dashboard_view(request):
     #display only today's flights
     schedules = FlightData.objects.filter(sd_date_utc=today)
     return render(request, 'aimsintegration/dashboard.html', {'schedules': schedules})
-
-
-
-
-
-def search_flights(request):
-    query = request.GET.get('query', '')
-    today = date.today()
-    
-    # Filter flights based on today's date and the search query for flight number or any other field
-    flights = FlightData.objects.filter(
-        sd_date_utc=today,
-        flight_no__icontains=query
-    )[:10]  # Limit results for performance
-    
-    # Prepare data for JSON response
-    flight_data = [
-        {
-            'sd_date_utc': flight.sd_date_utc.strftime('%Y-%m-%d'),
-            'flight_no': flight.flight_no,
-            'dep_code_iata': flight.dep_code_iata,
-            'dep_code_icao': flight.dep_code_icao,
-            'arr_code_iata': flight.arr_code_iata,
-            'arr_code_icao': flight.arr_code_icao,
-            'std_utc': flight.std_utc.strftime('%H:%M'),
-            'atd_utc': flight.atd_utc.strftime('%H:%M') if flight.atd_utc else '',
-            'takeoff_utc': flight.takeoff_utc.strftime('%H:%M') if flight.takeoff_utc else '',
-            'touchdown_utc': flight.touchdown_utc.strftime('%H:%M') if flight.touchdown_utc else '',
-            'ata_utc': flight.ata_utc.strftime('%H:%M') if flight.ata_utc else '',
-            'sta_utc': flight.sta_utc.strftime('%H:%M') if flight.sta_utc else '',
-        }
-        for flight in flights
-    ]
-    return JsonResponse({'flights': flight_data})
