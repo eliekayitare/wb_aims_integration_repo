0e7b0b0e3dd53fc89776979f27e6fb25a2b52238
eliekayitare <kayitarelie@gmail.com>
2024-11-06 16:24:51 +0200

    new commit

--- aimsintegration/urls.py
+++ aimsintegration/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('search-flights/', views.search_flights, name='search_flights'),  # Add this line
 ]
