b58f2ef331d0263a5c24507d8261d35c26d617a7
eliekayitare <kayitarelie@gmail.com>
2024-11-23 16:55:14 +0200

    new commit

--- aimsintegration/utils.py
+++ aimsintegration/utils.py
@@ -179,6 +179,9 @@ def process_flight_schedule_file(attachment):
             # Skip line if insufficient fields
             if len(fields) < 9:
                 logger.error(f"Skipping line {line_num} due to insufficient fields: {fields}")
+                print("----------------------------------------------")
+                print(len(fields))
+                print("----------------------------------------------")
                 continue
 
             try:
