dd16f0945340ad4bc37057ba399445e97fc93b62
rwakatechhub <rwakatechhub@gmail.com>
2025-08-25 17:22:29 +0200

    new commit

--- flightops/settings.py
+++ flightops/settings.py
@@ -360,12 +360,12 @@ CELERY_BEAT_SCHEDULE = {
      # Job 97: fetch flight-crew assignments every 10 minutes
     'fetch-job97-every-10-minutes': {
         'task': 'aimsintegration.tasks.fetch_job97',
-        'schedule': crontab(minute='*/3'),
+        'schedule': crontab(minute='*/1'),
     },
     # Job 1008: fetch static crew details every 15 minutes
     'fetch-job1008-every-15-minutes': {
         'task': 'aimsintegration.tasks.fetch_job1008',
-        'schedule': crontab(minute='*/4'),
+        'schedule': crontab(minute='*/10'),
     },
 
 }
