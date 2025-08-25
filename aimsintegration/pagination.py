9786ace838a94a3c09a51874eb6cdcd33c553744
rwakatechhub <rwakatechhub@gmail.com>
2025-08-25 14:42:46 +0200

    no limit

--- aimsintegration/pagination.py
+++ aimsintegration/pagination.py
@@ -4,7 +4,8 @@ from rest_framework.response import Response
 class FlexiblePageSizePagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     page_size = 10  # Default page size
-    max_page_size = 100  # Maximum allowed page size
+    # max_page_size = 100  # Maximum allowed page size
+    max_page_size = None 
     
     def get_paginated_response(self, data):
         return Response({
