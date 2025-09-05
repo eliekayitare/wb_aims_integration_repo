9da5223b946ef1555ea2d380dea08aa43aa0f7ed
Elie <kayitarelie@gmail.com>
2025-09-05 18:22:49 +0200

    ne commit

--- aimsintegration/tasks.py
+++ aimsintegration/tasks.py
@@ -1934,7 +1934,7 @@ def delete_emails_by_subject_list(self):
     """
     # Specify the subject patterns to delete
     subject_patterns = [
-        "Altea Flight Management",
+        "Load Message Edno",
         "Altea CM",
         "MVT",
         "Met",
